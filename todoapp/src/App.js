94c4d403788890583964ec7e23eee4d980e4aaec
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2020-10-21 00:34:40 +0530

    todoapp: Phase 6 + Phase 6.1

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -9,9 +9,29 @@ class App extends React.Component {
       this.state = {
           todos : TodosData
       }
+      this.handleChange = this.handleChange.bind(this)
   }
+
+  handleChange(id) {
+    this.setState(prevState => {
+        const updatedTodos = prevState.todos.map(todo => {
+            if (todo.id === id) {
+                return {
+                    ...todo,
+                    completed : !todo.completed
+                }
+            }
+            return todo
+        })
+        return {
+            todos : updatedTodos
+        }
+    })
+}
+
   render() {
-      const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
+      const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} 
+        handleChange={this.handleChange} />)
 
       return (
           <div className="todo-list">
